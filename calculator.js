db1c5dbd49ccb2fe1d173155147e8c98cc78367b
inan yigit <inanygt@hotmail.com>
2022-08-15 15:28:02 +0200

    finished calc exc

--- calculator.js
+++ calculator.js
@@ -6,19 +6,27 @@ const subtract = function(a,b) {
 	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce(function(sum, init) {
+    return sum += init;
+  }, 0)
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  return array.reduce((a,b) => a *= b, 1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
